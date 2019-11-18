e4ae9aae2669d9eb16586ec9a5f3702bc433aa3e
Sudhan Shahi <sudhan.shahi05@gmail.com>
2019-11-18 22:18:41 +0545

    Class component

--- monsters/src/App.js
+++ monsters/src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello, Sudhan Shahi
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      text: 'Hello, Sudhan!'
+    }
+  }
+  render (){
+    return(
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            {this.state.text}
+          </p>
+          <button onClick = {() => this.setState({text: 'Hello, Shahi!'})}>Click Me</button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
